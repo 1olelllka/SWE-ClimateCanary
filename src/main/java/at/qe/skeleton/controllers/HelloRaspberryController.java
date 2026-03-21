b0778dcab9660408d3a8949e53e980bdec6361f2
Diana Postupaieva <Diana.Postupaieva@student.uibk.ac.at>
2026-03-21 15:49:46 +0100

    add end-point to forward receive msg to frontend

--- src/main/java/at/qe/skeleton/controllers/HelloRaspberryController.java
+++ src/main/java/at/qe/skeleton/controllers/HelloRaspberryController.java
@@ -2,35 +2,46 @@ package at.qe.skeleton.controllers;
 
 import at.qe.skeleton.dtos.RaspberryCommandDTO;
 import at.qe.skeleton.dtos.RaspberryTestDTO;
-import at.qe.skeleton.dtos.TestInfoDTO;
 import at.qe.skeleton.feign.TestConnectionClient;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class HelloRaspberryController {
-
-    @Autowired
-    private TestConnectionClient connectionClient;
-
-    @GetMapping("/test-raspberry")
-    public ResponseEntity<RaspberryTestDTO> getRaspberryResponse() {
-        ResponseEntity<RaspberryTestDTO> response = connectionClient.postCommandToRaspberry(new RaspberryCommandDTO("turn_on_led"));
+    private static final Logger logger = LoggerFactory.getLogger(HelloRaspberryController.class);
+    private final TestConnectionClient connectionClient;
+    // placeholder for PiMessage (is overwritten as soon as message received)
+    private RaspberryTestDTO latestPiMessage = new RaspberryTestDTO("");
+    public HelloRaspberryController(TestConnectionClient connectionClient) {
+        this.connectionClient = connectionClient; // Spring automatically injects this
+    }
+    
+    // Frontend POSTs a message -> forwards to Pi
+    @PostMapping("/send-to-raspberry")
+    public ResponseEntity<Void> sendToPi(@RequestBody RaspberryCommandDTO command) {
+        ResponseEntity<RaspberryTestDTO> response = connectionClient.postCommandToRaspberry(command);
+        logger.info("Server leitet Nachricht weiter an Pi");
         if (response.getStatusCode().value() >= 300) {
             return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
         }
-        return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    //Pi POSTs its message here
     @PostMapping("/test-info")
-    public ResponseEntity<TestInfoDTO> receiveMsg() {
-        System.out.println("...API call performed...");
-        return new ResponseEntity<>(new TestInfoDTO("Hello World!!!"), HttpStatus.OK);
+    public ResponseEntity<Void> receiveMsg(@RequestBody RaspberryTestDTO dto) {
+        this.latestPiMessage = dto;
+        logger.info("Server received from Pi: {}", dto.message());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // Frontend GETs the latest Pi message
+    @GetMapping("/test-raspberry")
+    public ResponseEntity<RaspberryTestDTO> getRaspberryResponse(){
+        return new ResponseEntity<>(latestPiMessage, HttpStatus.OK); //returns whatever Pi last sent
     }
 }
--- src/main/java/at/qe/skeleton/dtos/RaspberryTestDTO.java
+++ src/main/java/at/qe/skeleton/dtos/RaspberryTestDTO.java
@@ -1,7 +1,6 @@
 package at.qe.skeleton.dtos;
 
 public record RaspberryTestDTO(
-        String status,
         String message
 ) {
 }
