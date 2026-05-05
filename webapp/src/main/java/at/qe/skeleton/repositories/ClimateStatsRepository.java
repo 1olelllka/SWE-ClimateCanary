a51df636f1ef378efbc49899a5c9b9ffb5096c7f
Oleh Sichko <oleh.sichko@student.uibk.ac.at>
2026-05-05 12:58:19 +0200

    Changed the folder structure for webapp

--- webapp/src/main/java/at/qe/skeleton/repositories/ClimateStatsRepository.java
+++ webapp/src/main/java/at/qe/skeleton/repositories/ClimateStatsRepository.java
@@ -4,7 +4,6 @@ import at.qe.skeleton.model.ClimateStats;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
