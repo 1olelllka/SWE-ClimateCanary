d45d22b2ce0797e96fa475c5de002f9331fd63a0
Matthias Thomas Tiefenthaler <matthias.tiefenthaler@student.uibk.ac.at>
2026-05-14 00:36:03 +0200

    feat: add dark mode

--- webapp/src/main/frontend/vite-env.d.ts
+++ webapp/src/main/frontend/vite-env.d.ts
@@ -1,1 +1,11 @@
 declare const REACT_APP_BACKEND_SERVER_URL: string;
+
+declare module '*.css?url' {
+    const url: string;
+    export default url;
+}
+
+declare module '*.png' {
+    const src: string;
+    export default src;
+}
