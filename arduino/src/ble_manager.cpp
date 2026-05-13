04b3aee73de42983e16828c3828e95536d3dfc59
Jakob Salomon <jakob.salomon@student.uibk.ac.at>
2026-05-13 16:53:47 +0200

    fix: undid previous changes, so DEVICE_PREFIX -> DEVICE_NAME again

--- arduino/src/ble_manager.cpp
+++ arduino/src/ble_manager.cpp
@@ -17,8 +17,8 @@ bool BLEManager::begin(DisplayManager* display) {
   }
 
   BLE.setAdvertisingInterval(ADVERTISING_INTERVAL);
-  BLE.setLocalName(DEVICE_PREFIX);
-  BLE.setDeviceName(DEVICE_PREFIX);
+  BLE.setLocalName(DEVICE_NAME);
+  BLE.setDeviceName(DEVICE_NAME);
   BLE.setAdvertisedService(service);
 
   service.addCharacteristic(txCharacteristic);
