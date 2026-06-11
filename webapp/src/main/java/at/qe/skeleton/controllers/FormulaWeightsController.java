36bbc3a11b990bf31362f24d69019e0a09c9fcc5
Oleh Sichko <oleh.sichko@student.uibk.ac.at>
2026-06-11 12:37:01 +0200

    Added api documentation to new endpoints

--- webapp/src/main/java/at/qe/skeleton/controllers/FormulaWeightsController.java
+++ webapp/src/main/java/at/qe/skeleton/controllers/FormulaWeightsController.java
@@ -4,9 +4,16 @@ import at.qe.skeleton.dtos.FormulaWeightCreateDTO;
 import at.qe.skeleton.dtos.FormulaWeightDTO;
 import at.qe.skeleton.exceptions.ValidationException;
 import at.qe.skeleton.services.FormulaWeightService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -16,15 +23,28 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@Tag(name = "Formula Weights Configuration")
 public class FormulaWeightsController {
 
     private final FormulaWeightService service;
 
+    @Operation(summary = "Get current formula weights. CAN_MANAGE_WEIGHT_FORMULA permission required")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Current formula."),
+            @ApiResponse(responseCode = "403", description = "Unauthorized.", content = @Content())
+    })
     @GetMapping("/weights")
     ResponseEntity<FormulaWeightDTO> getCurrentFormulaWeights() {
         return new ResponseEntity<>(service.getFormulaWeight(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Update current formula weights. CAN_MANAGE_WEIGHT_FORMULA permission required")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated formula."),
+            @ApiResponse(responseCode = "400", description = "Validation error.",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))),
+            @ApiResponse(responseCode = "403", description = "Unauthorized.", content = @Content())
+    })
     @PatchMapping("/weights")
     ResponseEntity<FormulaWeightDTO> patchCurrentFormulaWeights(@RequestBody @Valid  FormulaWeightCreateDTO dto,
                                                                 BindingResult bindingResult) {
